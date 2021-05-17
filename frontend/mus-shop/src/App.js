dc8277bea5bd6163da2a1377c41a5e0ee54e20b8
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-05-16 19:24:49 -0700

    added react-bootstrap; created card component, footer component and header component.

--- frontend/mus-shop/src/App.js
+++ frontend/mus-shop/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
+import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
+import HomePage from './pages/home/home.component'
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to MusShop</h1>
+      <Header/>
+        <main className='pa-3'>
+        <Container>
+          <HomePage />
+        </Container>
+        </main>
+      <Footer/>
     </>
   );
 }

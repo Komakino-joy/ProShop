e5307ac448807d90f0bc90f37e996b8b47fbbf8b
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-05-16 20:34:01 -0700

    added bootstrap router dom and react router dom to handle links.

--- frontend/mus-shop/src/App.js
+++ frontend/mus-shop/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import { Container } from 'react-bootstrap';
 
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
-import HomePage from './pages/home/home.component'
+import HomePage from './pages/home-page/home-page.component'
+import ProductPage from './pages/product-page/product-page.component';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header/>
         <main className='pa-3'>
         <Container>
-          <HomePage />
+          <Route exact path='/' component={HomePage}  />
+          <Route path='/product/:id' component={ProductPage}  />
         </Container>
         </main>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
